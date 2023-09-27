67bb2afb850363ac77b4131434022b2738fad6bf
dino <dino.lange@gmail.com>
2023-09-27 17:43:31 +0200

    database access

--- main.go
+++ main.go
@@ -2,73 +2,49 @@ package main
 
 import (
 	"database/sql"
-	"errors"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type Page struct {
+	id    int
 	Title string
-	Body  []byte
+	Body  string
 }
 
 var templates = template.Must(template.ParseFiles("templates/edit.html", "templates/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile("data/"+filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := "data/" + title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+	p, err := getPageByTitle(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-	renderTemplate(w, "view", p)
+	renderTemplate(w, "view", &p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+	p, err := getPageByTitle(title)
 	if err != nil {
-		p = &Page{Title: title}
+		p = Page{Title: title}
 	}
-	renderTemplate(w, "edit", p)
+	renderTemplate(w, "edit", &p)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
+	_, err := saveBodyForTitle(title, body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-	m := validPath.FindStringSubmatch(r.URL.Path)
-	if m == nil {
-		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
-	}
-	return m[2], nil // The title is the second subexpression.
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
@@ -90,30 +66,55 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "data/main.db")
-	if err != nil {
-		log.Panic(err)
-	}
-	sqlStmt := "SELECT * FROM entries;"
-	rows, err := db.Query(sqlStmt)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	var id int
-	var text sql.NullString
-	var name sql.NullString
-
-	for rows.Next() {
-		err := rows.Scan(&id, &text, &name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(id, text.String, name)
-	}
-
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getPageByTitle(title string) (Page, error) {
+	db, err := sql.Open("sqlite3", "data/main.db")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var body Page
+	row := db.QueryRow("SELECT body, title FROM entries WHERE title = ?", title)
+	if err := row.Scan(&body.Body, &body.Title); err != nil {
+		return body, err
+	}
+
+	return body, nil
+}
+
+func saveBodyForTitle(title string, body string) (int64, error) {
+	db, err := sql.Open("sqlite3", "data/main.db")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = getPageByTitle(title)
+
+	if err == sql.ErrNoRows {
+		result, err := db.Exec("INSERT INTO entries (title, body) VALUES (?,?)", title, body)
+		if err != nil {
+			return 0, fmt.Errorf("addAlbum: %v", err)
+		}
+		id, err := result.LastInsertId()
+		if err != nil {
+			return 0, fmt.Errorf("addAlbum: %v", err)
+		}
+		return id, nil
+	}
+
+	result, err := db.Exec("UPDATE entries SET body = ? WHERE title = ?", body, title)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
+
 }
